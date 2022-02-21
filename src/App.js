48023477132e116813a2946da7203702be54c9eb
manav shah <manav.march12@gmail.com>
2022-02-21 12:10:31 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './button';
+import Inputtext from './textbox';
+import Lottie from 'lottie-web';
+import React,{useEffect, useRef} from 'react';
+import logo from './image/animation.gif'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <p>It's my first react app</p>
+        <p>Login</p>
+        <img src={logo} alt="loging" width="300" height="300"></img>        
+        <div><Inputtext title="Username"></Inputtext></div>
+        <div><Inputtext title="Email"></Inputtext></div>
+        <div><Inputtext title="Password"></Inputtext></div>
+        <div><Inputtext title="Confirm Password"></Inputtext></div>
+        <Button title="Submit"></Button>
+        <Button title="Signup"></Button>
+        </div>
+      
       </header>
     </div>
   );
---
+++ src/textbox.jsx
@@ -0,0 +1,5 @@
+export default function Button(props){
+
+    return <input type="text" id="fname" name="fname" className="inputtexts" placeholder={props.title} ></input>
+
+}
